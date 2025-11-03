0be118c970b07c47137702a0bbe1d9520148a871
ThiagoEmanoel25 <thiagoemanoel181@gmail.com>
2025-11-03 17:13:13 -0300

    feat: criação da dimensão das tabelas

---
+++ gerenciador-orcamento/src/Views/BudgetTable.jsx
@@ -0,0 +1,22 @@
+export default function BudgetTable({ data }) {
+  return (
+    <table className="w-full border-collapse border border-gray-300">
+      <thead>
+        <tr className="bg-gray-200">
+          <th className="border px-4 py-2">Categoria</th>
+          <th className="border px-4 py-2">Quantidade (R$)</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((item, i) => (
+          <tr key={i}>
+            <td className="border px-4 py-2">{item.categoria}</td>
+            <td className="border px-4 py-2 text-right">
+              {item.quantidade.toFixed(2)}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
