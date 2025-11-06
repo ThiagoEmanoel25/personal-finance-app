a6af1993007c77891421d0f476ce3ca5d69d68a6
ThiagoEmanoel25 <thiagoemanoel181@gmail.com>
2025-11-06 13:41:14 -0300

    Feach: criação dos modulos

---
+++ gerenciador-orcamento/src/Views/index.js
@@ -0,0 +1,3 @@
+export { default as BudgetForm } from "./BudgetForm";
+export { default as BudgetTable } from "./BudgetTable";
+export { default as BudgetStatsDashboard } from "./BudgetStatsDashboard";
