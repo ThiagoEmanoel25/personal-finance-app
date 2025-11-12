a9dc6b363ace098abf9f11308bb23d2210e13c02
ThiagoEmanoel25 <thiagoemanoel181@gmail.com>
2025-11-12 12:49:13 -0300

    feach: transformando os export em modulos

---
+++ gerenciador-orcamento/src/Views/index.js
@@ -0,0 +1,3 @@
+export { default as BudgetForm} from "./BudgetForm.jsx";
+export { default as BudgetTable} from "./BudgetTable.jsx";
+export { default as BudgetStatsDashboard} from "./BudgetStatsDashboard.jsx";
