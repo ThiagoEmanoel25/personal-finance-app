50ff26361e38bd7ec0a853a02a7fde1c79e62a6e
ThiagoEmanoel25 <thiagoemanoel181@gmail.com>
2025-11-12 12:54:32 -0300

    fix: consertando a nomenclatura dos controllers

--- gerenciador-orcamento/src/controllers/BudgetController.js
+++ gerenciador-orcamento/src/controllers/BudgetController.js
@@ -1,14 +1,15 @@
-// cordena o Model com a view
-import { getbudgetData, addbudgetRow } from "../models/budgetmodels";
+import { getBudgetData, addBudgetRow } from "../models/budgetmodels.js";
 
-
-export async function LoadBudgetData(setBudgetData) {
-
-    const data = await getbudgetData();
-    setBudgetData(data);
+export async function loadBudgetData(setBudgetData) {
+  const data = await getBudgetData();
+  setBudgetData(data);
 }
 
 export async function handleAddRow(setBudgetData, categoria, quantidade) {
   const newItem = await addBudgetRow(categoria, quantidade);
-  setBudgetData(prev => [...prev, newItem]);
+  setBudgetData((prev) => [...prev, newItem]);
+}
+
+export function removeBudgetRow(setBudgetData, index) {
+  setBudgetData((prev) => prev.filter((_, i) => i !== index));
 }
