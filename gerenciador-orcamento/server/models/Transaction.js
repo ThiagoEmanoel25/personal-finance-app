5208af3d03ba1ce4dc4360ffa6a57f49fa9dcd61
ThiagoEmanoel25 <thiagoemanoel181@gmail.com>
2025-12-26 15:58:01 -0300

    feat: implement core user authentication, transaction management, and dashboard features with new models, views, and controllers.

---
+++ gerenciador-orcamento/server/models/Transaction.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const TransactionSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true, 
+    enum: ["Entrada", "Saída"],
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  value: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Transaction", TransactionSchema);
