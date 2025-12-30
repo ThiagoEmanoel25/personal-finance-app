eb6143977c40825467dc96bd787aa62694ff41da
ThiagoEmanoel25 <thiagoemanoel181@gmail.com>
2025-12-30 15:50:52 -0300

    feat: Add BalanceChart and Profile views, and export them from the views index.

--- gerenciador-orcamento/src/Views/index.js
+++ gerenciador-orcamento/src/Views/index.js
@@ -7,3 +7,5 @@ export { default as Login } from "./Login.jsx";
 export { default as Register } from "./Register.jsx";
 export { default as Dashboard } from "./Dashboard.jsx";
 export { default as GamificationCard } from "./GamificationCard.jsx";
+export { default as Profile } from "./Profile.jsx";
+export { default as Badges } from "./Badges.jsx";
